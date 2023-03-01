bef4e189849cff6e0560f6cd68ccf3e69b69e988
DRACOX <alexis.forestier2001@gmail.com>
2023-03-01 17:46:38 +0100

    update User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -51,7 +51,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, JsonSer
     private ?string $email = null;
 
     #[ORM\Column]
-    private array $roles = [];
+    private array $roles = ['ROLE_USER'];
 
     /**
      * @var string The hashed password
